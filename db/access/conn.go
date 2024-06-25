6f489ee0b6026d21aacfd3b1acf93ceec1e9c975
希嘉嘉 <hi@xijaja.com>
2024-06-25 19:12:10 +0800

    update conn

--- db/access/conn.go
+++ db/access/conn.go
@@ -29,14 +29,18 @@ func initSqlite() *gorm.DB {
 	if err != nil {
 		log.Fatal("读取 db.sqlite 文件失败")
 	}
+	// 数据库连接
 	var db *gorm.DB
-	db, dbErr := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
-	if err := db.Exec(string(dbFile)).Error; err != nil {
-		panic("链接 Sqlite 数据库恐慌：" + err.Error())
-	}
 	// 使用相对路径的 sqlite 数据库
 	// var db, dbErr = gorm.Open(sqlite.Open("db/access/db.sqlite"), &gorm.Config{})
+	// 使用内存的 sqlite 数据库
+	db, dbErr := gorm.Open(sqlite.Open("file::memory:?cache=shared"), &gorm.Config{})
+	// 数据库连接错误
 	if dbErr != nil {
+		panic("链接 Sqlite 数据库恐慌：" + dbErr.Error())
+	}
+	// 执行数据库文件，初始化数据库
+	if err := db.Exec(string(dbFile)).Error; err != nil {
 		panic("初始化 Sqlite 数据库恐慌：" + err.Error())
 	}
 	return db
